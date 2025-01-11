7878321d7e8e325fe8201b08ffa9694d228e9d66
spvcxzzzz <dart.to8645@gmail.com>
2025-01-11 14:12:28 +0500

    Fixed date, teacher name, time, added aud.
    4fut:
    #Remake item_week
    #grouping lessons in weekview
    ##add extra group view
    ###think on more settings

--- app/src/main/java/com/spvcxzzzz/nvsutimetable/ui/home/LessonAdapter.kt
+++ app/src/main/java/com/spvcxzzzz/nvsutimetable/ui/home/LessonAdapter.kt
@@ -7,6 +7,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.spvcxzzzz.nvsutimetable.R
 import com.spvcxzzzz.nvsutimetable.model.Lesson
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class LessonAdapter(private val lessons: List<Lesson>) : RecyclerView.Adapter<LessonAdapter.LessonViewHolder>() {
 
@@ -19,11 +22,50 @@ class LessonAdapter(private val lessons: List<Lesson>) : RecyclerView.Adapter<Le
     override fun onBindViewHolder(holder: LessonViewHolder, position: Int) {
         val lesson = lessons[position]
 
+        fun distantCheck(): String {
+            if (lesson.AUD == "Дистанционная") {
+                val answer = "Дистанционная"
+                return answer
+            } else {
+                val answer = lesson.AUD + " - " + lesson.Building + " корпус"
+                return answer
+            }
+        }
+
+        fun dateFromApiToView(inputDate: String): String {
+            val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
+            val outputFormatter = DateTimeFormatter.ofPattern("d MMMM", Locale("ru"))
+
+            val date = LocalDate.parse(inputDate, inputFormatter)
+            val formattedDate = date.format(outputFormatter)
+            return if (formattedDate != null) {
+                formattedDate
+            } else {
+                inputDate
+            }
+        }
+
+        fun formatTeacherName(): String {
+            // Декодируем Unicode строку
+            val decodedName = lesson.TEACHER
+
+            // Разбиваем строку на части
+            val parts = decodedName.split(" ")
+
+            // Если имя состоит из ФИО
+            return if (parts.size >= 3) {
+                "${parts[0]} ${parts[1].firstOrNull() ?: ""}.${parts[2].firstOrNull() ?: ""}."
+            } else {
+                decodedName // Если формат неправильный, возвращаем исходное имя
+            }
+        }
+
         // Заполняем данные в item_week.xml
         holder.disciplineTextView.text = lesson.DISCIPLINE
-        holder.teacherTextView.text = lesson.TEACHER
-        holder.dateTextView.text = lesson.DATEZAN
-        holder.timeTextView.text = "${lesson.TIMEZAN} - ${lesson.TIMEZAN_END}"
+        holder.teacherTextView.text = formatTeacherName()
+        holder.dateTextView.text = dateFromApiToView(lesson.DATEZAN)
+        holder.timeTextView.text = "${lesson.TIMEZAN?.takeIf { it.isNotEmpty() }?.substring(0, 5)} - ${lesson.TIMEZAN_END?.takeIf { it.isNotEmpty() }?.substring(0, 5)}"
+        holder.audTextView.text = distantCheck()
     }
 
     override fun getItemCount(): Int {
@@ -36,5 +78,6 @@ class LessonAdapter(private val lessons: List<Lesson>) : RecyclerView.Adapter<Le
         val teacherTextView: TextView = itemView.findViewById(R.id.tv_teacher)
         val dateTextView: TextView = itemView.findViewById(R.id.tv_date)
         val timeTextView: TextView = itemView.findViewById(R.id.tv_time)
+        val audTextView: TextView = itemView.findViewById(R.id.tv_aud)
     }
 }
